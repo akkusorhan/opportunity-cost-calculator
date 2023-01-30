b674fd54c69b23a0f82274232562b0d2e093f772
akkusorhan <akkusorhan2@gmail.com>
2023-01-30 08:28:14 -0500

    Update App.jsx

--- opportunity-cost-calculator/src/App.jsx
+++ opportunity-cost-calculator/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero"
 import axios from "axios"
 
 function App() {
+  //_______
   // Setting State
   const [stocks, setStocks] = React.useState("");
   const [list, setList] = React.useState([])
@@ -19,12 +20,24 @@ function App() {
     setList(prev => [...prev, listItem])
     setStocks("")
 
-    console.log(list)
+    console.log(list.map(item => item.value))
   }
+  //___________
+  // Setting State
+  const [timeHorizon, setTimeHorizon] = React.useState("")
+
+  // Helper Functions
+  function handleTimeChange(event) {
+    setTimeHorizon(prev => prev = event.target.name = event.target.value)
+
+    console.log(`time horizon changed to ${timeHorizon}`)
+  }
+
 
   return (
     <>
-
+    ___
+    <br></br>
       <input 
         type="text"
         placeholder="add stocks"
@@ -37,6 +50,22 @@ function App() {
       <ul>
         {list.map(item => <li key={item.id}>{item.value}</li>)}
       </ul>
+      <br></br>
+      ___
+      <br></br>
+      <select 
+        name="timeHorizon" 
+        id="timeHorizon"
+        value={timeHorizon}
+        onChange={handleTimeChange}
+      >
+        <option value="">-- choose --</option>
+        <option value="18 months">18 months</option>
+        <option value="24 months">24 months</option>
+        <option value="36 months">36 months</option>
+        <option value="48 months">48 months</option>
+
+      </select>
     </>
   )
 }
