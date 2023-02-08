fefdb0dc333774be1b22247396e6de9c6c49c181
akkusorhan <akkusorhan2@gmail.com>
2023-02-08 17:44:57 -0500

    updating components

--- opportunity-cost-calculator/src/App.jsx
+++ opportunity-cost-calculator/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Navbar from "./components/Navbar"
 import DateInput from './components/DateInput';
 import AmountSaved from "./components/AmountSaved"
 import TickerInput from './components/TickerInput';
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <>
+    <Navbar />
     <DateInput 
       timeHorizon={timeHorizon}
       setTimeHorizon={setTimeHorizon}
