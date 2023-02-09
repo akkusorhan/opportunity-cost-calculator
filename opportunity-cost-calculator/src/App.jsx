fb729bd3c43ffed8b849bbf7ee14b205241d20f1
akkusorhan <akkusorhan2@gmail.com>
2023-02-09 15:31:40 -0500

    updating components

--- opportunity-cost-calculator/src/App.jsx
+++ opportunity-cost-calculator/src/App.jsx
@@ -2,56 +2,17 @@ import React from 'react'
 import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import DateInput from './components/DateInput';
-import AmountSaved from "./components/AmountSaved"
-import TickerInput from './components/TickerInput';
 
 function App() {
-  const [symbolInput, setSymbolInput] = React.useState("");
-  const [symbolList, setSymbolList] = React.useState([]);
-  const [timeHorizonInput, setTimeHorizonInput] = React.useState("");
-  const [timeHorizon, setTimeHorizon] = React.useState("");
-  const [amountSavedInput, setAmountSavedInput] = React.useState("");
-  const [amountSaved, setAmountSaved] = React.useState("");
-  const [stockData, setStockData] = React.useState([]);
+
 
 
   return (
     <>
     <Navbar />
     <Hero />
-    <DateInput 
-      timeHorizon={timeHorizon}
-      setTimeHorizon={setTimeHorizon}
-      timeHorizonInput={timeHorizonInput}
-      setTimeHorizonInput={setTimeHorizonInput}
-    />
     <br/>
-    <AmountSaved 
-      amountSaved={amountSaved}
-      setAmountSaved={setAmountSaved}
-      amountSavedInput={amountSavedInput}
-      setAmountSavedInput={setAmountSavedInput}
-    />
-    <br/>
-    <TickerInput 
-      symbolInput={symbolInput} 
-      setSymbolInput={setSymbolInput} 
-      symbolList={symbolList}
-      setSymbolList={setSymbolList}
-      stockData={stockData}
-      setStockData={setStockData}
-      //passing amount saved
-      amountSaved={amountSaved}
-      setAmountSaved={setAmountSaved}
-      amountSavedInput={amountSavedInput}
-      setAmountSavedInput={setAmountSavedInput}
-      //passing date input
-      timeHorizon={timeHorizon}
-      setTimeHorizon={setTimeHorizon}
-      timeHorizonInput={timeHorizonInput}
-      setTimeHorizonInput={setTimeHorizonInput}
-    />
+    
     </>
   )
 }
--- opportunity-cost-calculator/src/components/Hero.jsx
+++ opportunity-cost-calculator/src/components/Hero.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React from 'react'
+//import Navbar from "./components/Navbar"
+import DateInput from './DateInput';
+import AmountSaved from "./AmountSaved"
+import TickerInput from './TickerInput';
 
-function Hero(props) {
+function Hero() {
+    const [symbolInput, setSymbolInput] = React.useState("");
+    const [symbolList, setSymbolList] = React.useState([]);
+    const [timeHorizonInput, setTimeHorizonInput] = React.useState("");
+    const [timeHorizon, setTimeHorizon] = React.useState("");
+    const [amountSavedInput, setAmountSavedInput] = React.useState("");
+    const [amountSaved, setAmountSaved] = React.useState("");
+    const [stockData, setStockData] = React.useState([]);
+
+
     return (
         <section className="hero-content">
             <h1 className="hero-main-text">Calculate Your Spending <br/>Opportunity Cost.</h1>
             <h3>See your spending dollars invested, over time, in your favorite stocks.</h3>
             <hr/>
+            <DateInput 
+                timeHorizon={timeHorizon}
+                setTimeHorizon={setTimeHorizon}
+                timeHorizonInput={timeHorizonInput}
+                setTimeHorizonInput={setTimeHorizonInput}
+            />
+            <AmountSaved 
+      amountSaved={amountSaved}
+      setAmountSaved={setAmountSaved}
+      amountSavedInput={amountSavedInput}
+      setAmountSavedInput={setAmountSavedInput}
+    />
+    <br/>
+    <TickerInput 
+      symbolInput={symbolInput} 
+      setSymbolInput={setSymbolInput} 
+      symbolList={symbolList}
+      setSymbolList={setSymbolList}
+      stockData={stockData}
+      setStockData={setStockData}
+      //passing amount saved
+      amountSaved={amountSaved}
+      setAmountSaved={setAmountSaved}
+      amountSavedInput={amountSavedInput}
+      setAmountSavedInput={setAmountSavedInput}
+      //passing date input
+      timeHorizon={timeHorizon}
+      setTimeHorizon={setTimeHorizon}
+      timeHorizonInput={timeHorizonInput}
+      setTimeHorizonInput={setTimeHorizonInput}
+    />
 
         </section>
     )
