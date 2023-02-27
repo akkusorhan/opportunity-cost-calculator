a0bb050fcb2289c0488438ea197a72a957cc7c8b
akkusorhan <akkusorhan2@gmail.com>
2023-02-27 17:15:11 -0500

    Update TickerInput.jsx

--- opportunity-cost-calculator/src/components/TickerInput.jsx
+++ opportunity-cost-calculator/src/components/TickerInput.jsx
@@ -51,10 +51,6 @@ function TickerInput({
                 const month = (today.getMonth() + 1).toString().padStart(2, '0');
                 const day = today.getDate().toString().padStart(2, '0');
                 const formattedDate = `${year}-${month}-${day}`;
-                
-
-
-                
 
 
                 const apiCall1 = await fetch(`https://api.polygon.io/v1/open-close/${stockTicker}/${timeHorizon}?adjusted=true&apiKey=MXrXoKsreyzlXOqFZZlKE3yGdbTlsieL`)
